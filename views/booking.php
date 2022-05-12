ebe346fbd89d6aba96a71e1174dab7ac1a8f1a42
maskadev <sidkiyouness@gmail.com>
2022-05-12 10:56:02 +0100

    add function for cancel before hours from the trip

--- views/booking.php
+++ views/booking.php
@@ -54,23 +54,20 @@
                 <?php 
                     $date_trip = date('Y-m-d h:i', strtotime($value['date'] . "  " .$value['depart_time']));
                     $date = new DateTime($date_trip);
-                    $trip_date_year = ($date->format('Y'));
                     $trip_date_month = ($date->format('m'));
                     $trip_date_day = ($date->format('d'));
-                    $trip_time = date_create($date->format('H:i'));
                     $cancel_available = false;
                     if(date("m") <= $trip_date_month) {
                         if(date("d") <= $trip_date_day){
-                            $currentTime = date_create(date("H:i"));
-                            $difference = date_diff($currentTime, $trip_time);
-                            $minutes = $difference->days * 24 * 60;
-                            $minutes += $difference->h * 60;
-                            $minutes += $difference->i;
-                            // echo $minutes . " ";
-                            if($minutes < 60) {
-                                $cancel_available = false;
-                            }else {
-                                $cancel_available = true;
+                            $start_date = new DateTime(date("Y-m-d H:i"));
+                            $since_start = $start_date->diff($date);                            
+                            // echo $since_start->h . " " . $since_start->i . "<br>";
+                            if($since_start->h > 0){
+                                if($since_start->i > 60) {
+                                    $cancel_available = false;
+                                }else {
+                                    $cancel_available = true;
+                                }
                             }
                         }
                         else {
@@ -82,7 +79,8 @@
                 ?>
                 <tbody>
                     <tr>
-                        <td><?= $value['depart_city'] ?></td>
+                        <!-- <td><?= $value['depart_city'] ?></td> -->
+                        <td><?= $value['id'] ?></td>
                         <td><?= $value['arrive_city'] ?></td>
                         <td><?= $value['date'] . "  " .$value['depart_time'] ?></td>
                         <td><a href="<?php if ($cancel_available == true){ echo 'http://localhost/Booking-train-ticket/booking/deleteBooking/' . $value['id'];} {echo "";}?>" class ="btn text-danger fw-bold" style="margin:0px; padding:0px">Cancel Trip</a></td>
